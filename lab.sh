2a3e9a1d3fb37e50539099a9080cf0ce3019ea86
JulioVS <julioer@me.com>
2026-04-24 15:25:15 -0300

    Added Databases and Tables

---
+++ lab.sh
@@ -0,0 +1,12 @@
+# Lab: Adding Databases and Tables
+
+# Key terms
+# Start MySQL Database
+# Getting Started
+
+touch app/src/main/resources/application.properties
+cd app
+mvn spring-boot:run
+
+# Insert Data into Tables
+# Conclusion and Next Steps
